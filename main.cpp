c613127133a87a5b8490bf672b7e4a9d9604e0af
spidersharma@gmail.com <spidersharma@gmail.com@fa6b0260-fc98-aa57-b35f-c9e340f9fcde>
2012-05-10 11:05:16 +0000

    Project Files

--- main.cpp
+++ main.cpp
@@ -1,20 +1,130 @@
 #include <iostream>
 
+#include <sys/time.h>
+#include<GLUT/glut.h>
+
 #include "Dirac2D/Dirac2D.h"
+
 USE_NAMESPACE_DIRAC2D
 
+void initScene();
+void changeSize(int w, int h);
+void renderScene(void);
 
-int main (int argc, char * const argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+PhysicalWorld* pWorld;
+
+void initScene()
+{
+	pWorld = new PhysicalWorld();
+	// Create Ground Body
+	PhysicalBody* pBodyGround = pWorld->createPhysicalBody();
+	pBodyGround->m_BodyType = EBT_STATIC;
+	PhysicalAppearance pApp;
+	dfloat groundWidth = 1.0f; dfloat groundHeight = 0.2f;
+	Vector2f vertices[4] = { Vector2f(groundWidth, groundHeight), Vector2f(-groundWidth, groundHeight), Vector2f(-groundWidth, -groundHeight), Vector2f(groundWidth, -groundHeight) };
+	pApp.m_CollisionAttributes.m_Shape = new RegularPolygon(vertices, 4);
+	pBodyGround->createPhysicalShape(pApp);
 	
-	Vector2f v(1,2);
-	Vector2f v1(1,2);
+	// Create Box
+	PhysicalBody* pBodyBox = pWorld->createPhysicalBody();
+	dfloat boxWidth = 0.2f; dfloat boxHeight = 0.2f;
+	Vector2f verticesBox[4] = { Vector2f(boxWidth, boxHeight), Vector2f(-boxWidth, boxHeight), Vector2f(-boxWidth, -boxHeight), Vector2f(boxWidth, -boxHeight) };
+	pApp.m_CollisionAttributes.m_Shape = new RegularPolygon(verticesBox, 4);
+	pBodyBox->createPhysicalShape(pApp);
 	
-	v *= v1;
-	v = v + v1;
 	
-	//CollisionManager cm(0);
+}
+
+void changeSize(int w, int h) 
+{
 	
-    return 0;
+    // Prevent a divide by zero, when window is too short
+    float ratio;
+    if(h == 0)
+        h = 1;
+	
+    ratio = 1.0* w / h;
+	
+    // Reset the coordinate system before modifying
+	
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	
+    // Set the viewport to be the entire window
+	
+    glViewport(0, 0, w, h);
+	
+    // Set the correct perspective.
+	
+    gluPerspective(45,ratio,1,1000);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0,0.0,5.0, 
+			  0.0,0.0,-1.0,
+			  0.0f,1.0f,0.0f);
+	
+	
+}
+
+void renderScene(void) 
+{	
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+	glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	glOrtho(-4, 4, -4, 4, -0.01, 100);
+	glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+	
+    glutSwapBuffers();
+}
+
+static void timerCallback (int value)
+{
+    timeval t1, t2;
+    static double elapsedTime;
+    double FPS = 0;
+    
+    static int frameCnt = 0;
+    
+    // start timer
+    gettimeofday(&t1, NULL);
+    
+    // do something
+    renderScene();
+    frameCnt ++;
+    
+    // stop timer
+    gettimeofday(&t2, NULL);
+    
+    // compute and print the elapsed time in millisec
+    elapsedTime += (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+    
+    if( frameCnt >=500 )
+    {
+        frameCnt=0;
+        FPS = 500 /(elapsedTime * 0.001);
+        printf("\n FPS = %f", FPS);
+        elapsedTime = 0;
+    }
+    
+    /* Call back again after elapsedUSecs have passed */
+    glutTimerFunc (0, timerCallback, 0);
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(512,512);
+    glutCreateWindow("Physics Engine - DIRAC2D");
+	glutDisplayFunc(renderScene);
+    //glutIdleFunc(renderScene);
+    glutReshapeFunc(changeSize);
+	glutTimerFunc(0, timerCallback, 0);
+	
+    glutMainLoop();
+	
+	return 0;
 }
