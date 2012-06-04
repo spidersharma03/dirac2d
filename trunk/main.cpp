1e3197acb6034e5f53177c4bb14f761e9daff1cd
spidersharma@gmail.com <spidersharma@gmail.com@fa6b0260-fc98-aa57-b35f-c9e340f9fcde>
2012-06-04 03:33:51 +0000

    Bug Fix in Circle  - EdgeChain Collision. still Buggy.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -565,7 +565,7 @@ void demo10()
 	pBodyGround->m_BodyType = EBT_STATIC;
 	
 	PhysicalAppearance pApp;
-	Vector2f vertices[5] = { Vector2f(-1.0f, 0.0f), Vector2f(-0.5f, 0.0f), Vector2f(0.0f, 0.0f)
+	Vector2f vertices[5] = { Vector2f(-1.0f, 0.0f), Vector2f(-0.5f, 0.15f), Vector2f(0.0f, 0.0f)
 		, Vector2f(0.5f, 0.0f), Vector2f(1.0f,0.0f) };
 	pApp.m_CollisionAttributes.m_Shape = new EdgeChain(vertices, 5);
 	pBodyGround->createPhysicalShape(pApp);
@@ -574,7 +574,7 @@ void demo10()
 	dfloat y = 0.5f;
 	PhysicalBody* pBodyCompound = pWorld->createPhysicalBody();
 	//pBodyCompound->m_BodyType = EBT_STATIC;
-	pBodyCompound->setPosition(Vector2f(0.0,y));
+	pBodyCompound->setPosition(Vector2f(0.35,y));
 	//pBodyCapsule->setAngle(PI_4*0.9);
 	dfloat capsuleRadius = 0.15f;
 	pApp.m_CollisionAttributes.m_Shape = new Circle(capsuleRadius);
