940b31aa34bd87ec2f67c666c8d5e9bee087bc31
spidersharma@gmail.com <spidersharma@gmail.com@fa6b0260-fc98-aa57-b35f-c9e340f9fcde>
2012-07-08 18:23:53 +0000

    Created test tag Version 1.

--- Dirac2D/dynamics/joints/PrismaticConstraint.h
+++ Dirac2D/dynamics/joints/PrismaticConstraint.h
@@ -34,9 +34,7 @@ public:
 	Vector2f m_ImpulseDirection;
 	Vector2f m_Anchor1, m_Anchor2; // Local Anchor Points
 	Vector2f m_r1, m_r2; // Radius Vectors wrt the Bodies centres.
-	dfloat m_EffectiveMass;
-	dfloat m_EffectiveMassParallel;
-	Matrix2f m_EffectiveMassMatrix;
+	Matrix3f m_EffectiveMassMatrix;
 	dfloat m_PositionError, m_PositionErrorParallel;
 	Vector2f m_WorldPerpendicularAxis;
 	Vector2f m_WorldAxis;
