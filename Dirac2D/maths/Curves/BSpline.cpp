cbe70223077e78a22df24a9efc16f38bcf3fbcd0
spidersharma@gmail.com <spidersharma@gmail.com@fa6b0260-fc98-aa57-b35f-c9e340f9fcde>
2012-10-07 09:34:43 +0000

    Spline

--- Dirac2D/maths/Curves/BSpline.cpp
+++ Dirac2D/maths/Curves/BSpline.cpp
@@ -7,35 +7,86 @@
  */
 
 #include "BSpline.h"
+#include "GLRenderer.h"
+#include "Vector2.h"
+
+USE_NAMESPACE_DIRAC2D
+
+GLRenderer* pglRenderer = new GLRenderer(0);
 
 void findOpenKnots(int nControlPoints, int nOrder, float* knotVector)
 {
+	for( int i=0; i<nOrder; i++ )
+		knotVector[i] = 0.0f;
+	for( int i=nOrder+nControlPoints-1; i>nOrder+nControlPoints-4; i-- )
+		knotVector[i] = nControlPoints - nOrder + 1;
+	int n = 1;
+	for( int i=nOrder; i<nOrder+nControlPoints-3; i++ )
+		knotVector[i] = n++;	
 }
 
 
-void splineBasis(int nControlPoints, int nOrder, float t, float* basisVector)
+void splineBasis(int nControlPoints, int nOrder, float t, float* knotVector, float* basisVector)
 {
-	float x[10];     // Knot Vector
-	float N_i1[10];  // First Order basis functions
+	float N_i1[40];  // First Order basis functions
 	
-	findOpenKnots(nControlPoints, nOrder, x);
 	
-	for (int i=0; i<nControlPoints; i++) 
+	// initialize the first order basis functions
+	for (int i=0; i<nControlPoints + nOrder - 1; i++) 
 	{
-		if( t >= x[i+1] || t < x[i] )
-			N_i1[i] = 0.0f;
-		else
+		if( t >= knotVector[i] && t < knotVector[i+1] )
 			N_i1[i] = 1.0f;
+		else
+			N_i1[i] = 0.0f;
 	}
 	
+	int N = nControlPoints + nOrder - 1;
+	// Calculate higher order basis functions
 	for( int o=1; o<nOrder; o++ )
 	{
-		for( int i=0; i<nControlPoints; i++ )
+		for( int i=0; i<N-1; i++ )
 		{
-			float sum = ( t - x[i] )/( x[i+nOrder-1] ) * N_i1[i] + ( x[i+nOrder] - t)/(x[i+nOrder] - x[i+1] ) *  N_i1[i+1];
+			float sum = ( t - knotVector[i] )/( knotVector[i+nOrder-1] ) * N_i1[i] + ( knotVector[i+nOrder] - t)/(knotVector[i+nOrder] - knotVector[i+1] ) *  N_i1[i+1];
 			basisVector[i] += sum;
 			N_i1[i] = sum;
 		}
+		N = N - 1;
+	}	
+}
+
+void BSpline(float* controlPoints, int nControlPoints, float* curvePoints, int nCurvePoints, int nOrder)
+{
+	float x[30];     // Knot Vector
+	float basis[30];     // Knot Vector
+
+	findOpenKnots(nControlPoints, nOrder, x);
+	float t = 0.0f;
+	float tStep = (float)(nControlPoints - nOrder + 1)/(nCurvePoints-1);
+	for( int i=0; i<2*nCurvePoints; i+=2 )
+	{
+		splineBasis( nControlPoints, nOrder, t, x, basis);
+		
+		float s1 = 0.0f;
+		float s2 = 0.0f;
+
+		for (int j=0; j<2*nControlPoints; j+=2) 
+		{
+			s1 += basis[j/2] * controlPoints[j];
+			s2 += basis[j/2] * controlPoints[j+1];
+		}
+		curvePoints[i]   = s1;
+		curvePoints[i+1] = s2;
+		t += tStep;
 	}
-	
+}
+
+void drawBSpline(float* curvePoints, int nCurvePoints)
+{
+	pglRenderer->setColor(0, 255, 0);
+	for( int i=0; i<2*nCurvePoints-2; i+=2 )
+	{
+		Vector2f p0(curvePoints[i], curvePoints[i+1]);
+		Vector2f p1(curvePoints[i+2], curvePoints[i+3]);
+		pglRenderer->drawLine(p0, p1);
+	}
 }
